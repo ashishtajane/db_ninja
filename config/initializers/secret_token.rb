3dc206a16bc21d1a18c4f308c92a4993dcfe50e6
Ashish Tajane <ashishtajane@gmail.com>
2014-05-22 16:07:45 +0530

    bootstrapping rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbNinja::Application.config.secret_token = 'c5499d632b05794efc84e94c9a7be7621fc2c966bcba134c08e9b3996fdd4e636159fc82199bc95d8bc9abceb5dd37f1e424ef06f535ac9b11955624b5219b89'
